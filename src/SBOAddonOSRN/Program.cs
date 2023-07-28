61468efa39a23f8b0d57c79a9119a91b03875806
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2023-07-28 22:51:17 +0500

    Bug fixed

--- src/SBOAddonOSRN/Program.cs
+++ src/SBOAddonOSRN/Program.cs
@@ -7,7 +7,6 @@ namespace SBOAddonOSRN
 	class Program
 	{
 		static SAPbobsCOM.Company oCom;
-		// static SAPbobsCOM.Recordset oRS;
 
 		[STAThread]
 		static void Main(string[] args)
@@ -57,67 +56,73 @@ namespace SBOAddonOSRN
 
 			if (pVal.FormTypeEx == "21" && pVal.BeforeAction == false && pVal.ItemUID == "Click" && pVal.EventType == SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED)
 			{
-				SAPbouiCOM.Form soForm = Application.SBO_Application.Forms.Item(FormUID);
-				SAPbouiCOM.Matrix oMatrix = (SAPbouiCOM.Matrix)soForm.Items.Item("43").Specific;
-				SAPbouiCOM.Matrix oMatrix2 = (SAPbouiCOM.Matrix)soForm.Items.Item("3").Specific;
-				SAPbouiCOM.DBDataSource matrixDT1 = soForm.DataSources.DBDataSources.Item("SBDR");
-
-				SAPbobsCOM.Recordset oRS = (SAPbobsCOM.Recordset)oCom.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
-
-
-				int rowCount = matrixDT1.Size;
-				int selectedRow = 0;
-
-				for (int i = 1; i <= rowCount; i++)
+				try
 				{
-					if (oMatrix.IsRowSelected(i))
+					SAPbouiCOM.Form soForm = Application.SBO_Application.Forms.Item(FormUID);
+					SAPbouiCOM.Matrix oMatrix = (SAPbouiCOM.Matrix)soForm.Items.Item("43").Specific;
+					SAPbouiCOM.Matrix oMatrix2 = (SAPbouiCOM.Matrix)soForm.Items.Item("3").Specific;
+					SAPbouiCOM.DBDataSource matrixDT1 = soForm.DataSources.DBDataSources.Item("SBDR");
+
+					SAPbobsCOM.Recordset oRS = (SAPbobsCOM.Recordset)oCom.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+
+
+					int rowCount = matrixDT1.Size;
+					int selectedRow = 0;
+
+					for (int i = 1; i <= rowCount; i++)
 					{
-						selectedRow = i;
-					}
-				}
-
-				SAPbouiCOM.EditText docQuan = (SAPbouiCOM.EditText)oMatrix.Columns.Item("37").Cells.Item(selectedRow).Specific;
-				var itemCode = ((SAPbouiCOM.EditText)oMatrix.Columns.Item("5").Cells.Item(selectedRow).Specific).Value.ToString();
-
-				Console.WriteLine("Itemcode: " + itemCode);
-				Console.WriteLine("DocQuan: " + docQuan.Value);
-
-				oRS.DoQuery($"SELECT T0.\"ItemCode\", Max(to_number(T0.\"DistNumber\")) FROM OSRN T0 WHERE T0.\"ItemCode\" = '{itemCode}' GROUP BY T0.\"ItemCode\"");
-
-
-				for (int i = 1; i <= int.Parse(docQuan.Value.ToString().Split('.')[0]); i++)
-				{
-					if (i > oMatrix2.RowCount)
-						oMatrix2.AddRow(1);
-
-					SAPbouiCOM.EditText mnfSerial = (SAPbouiCOM.EditText)oMatrix2.Columns.Item(1).Cells.Item(i).Specific;
-					SAPbouiCOM.EditText distNumber = (SAPbouiCOM.EditText)oMatrix2.Columns.Item(2).Cells.Item(i).Specific;
-					SAPbouiCOM.EditText date = (SAPbouiCOM.EditText)oMatrix2.Columns.Item("50").Cells.Item(i).Specific;
-
-
-					if (oRS.RecordCount > 0 && !oRS.EoF)
-					{
-						var dis = oRS.Fields.Item(1).Value.ToString();
-
-						if (string.IsNullOrEmpty(dis))
+						if (oMatrix.IsRowSelected(i))
 						{
-							distNumber.Value = $"{i}";
-							mnfSerial.Value = oRS.Fields.Item("ItemCode").Value.ToString() + $"_{i}";
+							selectedRow = i;
+						}
+					}
+
+					SAPbouiCOM.EditText docQuan = (SAPbouiCOM.EditText)oMatrix.Columns.Item("37").Cells.Item(selectedRow).Specific;
+					var itemCode = ((SAPbouiCOM.EditText)oMatrix.Columns.Item("5").Cells.Item(selectedRow).Specific).Value.ToString();
+
+					oRS.DoQuery($"SELECT T0.\"ItemCode\", Max(to_number(T0.\"DistNumber\")) FROM OSRN T0 WHERE T0.\"ItemCode\" = '{itemCode}' GROUP BY T0.\"ItemCode\"");
+
+					for (int i = 1; i <= int.Parse(docQuan.Value.ToString().Split('.')[0]); i++)
+					{
+						if (i > oMatrix2.RowCount)
+							oMatrix2.AddRow(1);
+
+						SAPbouiCOM.EditText mnfSerial = (SAPbouiCOM.EditText)oMatrix2.Columns.Item(1).Cells.Item(i).Specific;
+						SAPbouiCOM.EditText distNumber = (SAPbouiCOM.EditText)oMatrix2.Columns.Item(2).Cells.Item(i).Specific;
+						SAPbouiCOM.EditText date = (SAPbouiCOM.EditText)oMatrix2.Columns.Item("50").Cells.Item(i).Specific;
+
+						if (oRS.RecordCount > 0 && !oRS.EoF)
+						{
+							var dis = oRS.Fields.Item(1).Value.ToString();
+
+							if (string.IsNullOrEmpty(dis))
+							{
+								distNumber.Value = $"{i}";
+								mnfSerial.Value = itemCode + $"_{i}";
+							}
+							else
+							{
+								var number = (long.Parse(oRS.Fields.Item(1).Value.ToString()) + i).ToString();
+								Console.WriteLine(number);
+
+								distNumber.Value = (long.Parse(oRS.Fields.Item(1).Value.ToString()) + i).ToString();
+								mnfSerial.Value = itemCode + $"_{distNumber.Value}";
+							}
 						}
 						else
 						{
-							var number = (long.Parse(oRS.Fields.Item(1).Value.ToString()) + i).ToString();
-							Console.WriteLine(number);
-
-							distNumber.Value = (long.Parse(oRS.Fields.Item(1).Value.ToString()) + i).ToString();
-							mnfSerial.Value = oRS.Fields.Item("ItemCode").Value.ToString() + $"_{distNumber.Value}";
+							distNumber.Value = $"{i}";
+							mnfSerial.Value = itemCode + $"_{i}";
 						}
+
+
+						date.Value = DateTime.Now.ToString("yyyyMMdd");
 					}
-					date.Value = DateTime.Now.ToString("yyyyMMdd");
 				}
-
-
-
+				catch (Exception)
+				{
+					Application.SBO_Application.SetStatusBarMessage($"{oCom.GetLastErrorDescription()}", SAPbouiCOM.BoMessageTime.bmt_Medium);
+				}
 			}
 		}
 
